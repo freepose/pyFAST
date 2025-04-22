3e49ce0c8350ba9e031c5d13892726b53f8bd671
Zhijin Wang <freepose@126.com>
2025-04-23 07:46:36 +0800

    Delete clean.sh

--- clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'removing __pycache__ directories ... '
-find . -type d -name "__pycache__" -exec rm -rf {} \;
-
-echo 'removing .DS_Store files ... '
-find . -type f -name ".DS_Store" -exec rm -f {} \;
